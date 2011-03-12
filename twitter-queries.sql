e721961485994c0173e74f0b2ce7c17d3c90c430
Jaideep Bajwa <j.bajwa08@gmail.com>
2011-03-11 20:38:41 -0500

    part 1 and 4 modified

--- twitter-queries.sql
+++ twitter-queries.sql
@@ -1,11 +1,27 @@
 -- 1
+-- A<---->B
+DROP VIEW IF EXISTS A1 , A2 , A3 cascade;
 
-CCREATE VIEW AX AS(
- SELECT * FROM followers
+CREATE VIEW A1 AS(
+ SELECT j.username AS A, k.username AS B
+FROM followers j, followers k 
+WHERE j.followers = k.username AND k.followers = j.username 
 );
-SELECT DISTINCT j.username AS A, k.username AS B
-FROM followers j, AX k 
-WHERE j.followers = k.username AND k.followers <> j.username ;
+
+-- A---->B
+
+CREATE VIEW A2 AS(
+SELECT j.username AS A, k.username AS B
+FROM followers j, followers k 
+WHERE j.followers = k.username
+);
+
+-- A<--/-->B
+CREATE VIEW A3 AS(
+SELECT * FROM A2 EXCEPT SELECT * FROM A1
+);
+
+
 
 -- 2
 CREATE VIEW A1 AS(
@@ -35,3 +51,11 @@ select username AS use , (up - down) AS pop from A3, A4 WHERE username=followers
 );
 
 select username, name, location,pop from profile, R1 WHERE use = username ORDER BY pop DESC;
+
+-- 4
+
+DROP VIEW IF EXISTS A1 cascade;
+CREATE VIEW A1 AS (
+SELECT a.username , count(DISTINCT c.followers) AS num_of_diff_users FROM followers a ,followers b, followers c WHERE a.followers = b.username AND b.followers = c.username GROUP BY a.username ORDER BY num_of_diff_users DESC
+);
+
