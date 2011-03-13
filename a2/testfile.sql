dd1f5d7eea34d231dbd606c6a62d606b5879bd03
Zeeshan Qureshi <zeeshan@zqureshi.in>
2011-03-12 21:36:01 -0500

    delete extra stuff

--- a2/testfile.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO exam values ('2','1','1','23','2','2011-11-11');
-INSERT INTO exam values ('4','2','3','23','4','2011-11-11');
-INSERT INTO exam values ('6','3','5','11','7','2011-11-14');
-INSERT INTO exam values ('11','5','7','11','9','2011-11-12');
-INSERT INTO exam values ('12','5','7','11','9','2011-11-13');
-INSERT INTO technician values ('23','Q','z','ee');
-INSERT INTO technician values ('11','b','j','jj');
