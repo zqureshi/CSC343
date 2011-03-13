a0cf3a19b6f038c3c112e91cd44e6223cbf9b67d
Zeeshan Qureshi <zeeshan@zqureshi.in>
2011-03-12 19:41:05 -0500

    rollback ultra-schema.sql

--- a2/ultra-schema.sql
+++ a2/ultra-schema.sql
@@ -53,7 +53,11 @@ CREATE TABLE Exam (
 	tID INT NOT NULL,
 	dID INT NOT NULL,
 	schedule TIMESTAMP NOT NULL,
-	PRIMARY KEY (eID)
+	PRIMARY KEY (eID),
+	FOREIGN KEY (pID) REFERENCES Patient(OHIP),
+	FOREIGN KEY (cID) REFERENCES Clinic(cID),
+	FOREIGN KEY (tID) REFERENCES Technician(ID),
+	FOREIGN KEY (dID) REFERENCES Doctor(billingNum)	
 );
 
 -- A measurement that may be taken during a medical exam, with
@@ -99,5 +103,8 @@ CREATE TABLE Birth (
 	birthday DATE NOT NULL,
 	weight FLOAT NOT NULL,
 	attending INT,
-	PRIMARY KEY (mother, prStart)
+	PRIMARY KEY (mother, prStart),
+	FOREIGN KEY (mother) REFERENCES Patient(OHIP),
+	FOREIGN KEY (mother, prStart) REFERENCES Pregnancy (pID, start),
+	FOREIGN KEY (attending) REFERENCES Doctor(billingNum)
 );
